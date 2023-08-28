94451a0400a3afd5becd57c6ce4bf7fc3f332771
Yousuf Almandhari <yosif2015.m@gmail.com>
2023-08-28 17:35:50 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,20 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!isValid(username)) {
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(404).json({message: "Unable to register user."});
+
 });
 
 
@@ -61,18 +73,7 @@ public_users.get('/review/:isbn',function (req, res) {
 
     const book = books[isbn];
 
-    return res.status(300).json({message: book.reviews});
-
-    // for (const bookId in books) {
-    //     if (books.hasOwnProperty(bookId)) {
-    //       const book = books[bookId];
-    //       if (book.title === title) {
-    //         return res.status(300).json({message: book});
-    //       }
-    //     }
-    //   }
-
-    
+    return res.status(300).json({message: book.reviews});    
 });
 
 module.exports.general = public_users;
