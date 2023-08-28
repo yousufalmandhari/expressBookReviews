08fa5fef5fd30f731f98d75bd0cc25b31bca9bed
Yousuf Almandhari <yosif2015.m@gmail.com>
2023-08-28 16:36:58 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,34 +10,69 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: JSON.stringify(books,null,4)});
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    return res.status(300).json({message: books[isbn]});
  });
-  
+
+
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+
+    for (const bookId in books) {
+        if (books.hasOwnProperty(bookId)) {
+          const book = books[bookId];
+          if (book.author === author) {
+            return res.status(300).json({message: book});
+          }
+        }
+      }
+  
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+
+    for (const bookId in books) {
+        if (books.hasOwnProperty(bookId)) {
+          const book = books[bookId];
+          if (book.title === title) {
+            return res.status(300).json({message: book});
+          }
+        }
+      }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+
+    const book = books[isbn];
+
+    return res.status(300).json({message: book.reviews});
+
+    // for (const bookId in books) {
+    //     if (books.hasOwnProperty(bookId)) {
+    //       const book = books[bookId];
+    //       if (book.title === title) {
+    //         return res.status(300).json({message: book});
+    //       }
+    //     }
+    //   }
+
+    
 });
 
 module.exports.general = public_users;
